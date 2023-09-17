e8556fbc00889687b1e7123ea12b4fb2d04aba81
Touseef Iqbal <136140163+touseefiqbal845@users.noreply.github.com>
2023-09-17 14:28:32 +0500

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,16 +12,16 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => {
   return (
    <footer >
-    <a href='#' className='footer_logo'>Touseef Iqbal</a>
+{/*     <a href='#' className='footer_logo'>Touseef Iqbal</a> */}
 
-    <ul className='permalinks'>
+{/*     <ul className='permalinks'>
     <li><a href='#'>Home</a></li>
     <li><a href='#about'>About</a></li>
     <li><a href='#contact'>Contact</a></li>
     <li><a href='#experience'>Experience</a></li>
     <li><a href='#portfolio'>Portfolio</a></li>
        <li><a href='#testimonials'>Education</a></li>
-    </ul>
+    </ul> */}
    <div className='footer_social'>
     <a href='https://github.com/touseefiqbal845' target="_blank"><BsGithub/></a>
       <a href='https://www.linkedin.com/in/touseef-iqbal-836400209' target="_blank"><BsLinkedin/></a>
