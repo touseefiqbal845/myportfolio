5a5f84ef386ab318e7af7f7362a8e3da5252e1b7
Touseef Iqbal <136140163+touseefiqbal845@users.noreply.github.com>
2023-11-08 23:16:18 +0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
 
 <br />
 
-<h2 className="header_text m-3">Full Stack =[MERN Learning]</h2>
+<h2 className="header_text m-3">Web Developer</h2>
 
              
           <CTA /> 
